9ac329ee0faf287adf0d36f2a30294f0fa042b07
tush4r <stushar52@gmail.com>
2016-08-05 15:54:59 +0530

    Network Constants

---
+++ EdBinx/EdBinx/NetworkDefaults.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkDefaults.swift
+//  EdBinx
+//
+//  Created by Tushar Sharma on 05/08/16.
+//  Copyright © 2016 edbinx. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstants {
+    
+    /*API Docs are at this URL: http://v2.wp-api.org/reference/posts/ ,plugin is currently in the BETA phase and hence the documentation isn't final.*/
+    let baseURL = "http://edbinx.com/wp-json/wp/v2/"
+}
