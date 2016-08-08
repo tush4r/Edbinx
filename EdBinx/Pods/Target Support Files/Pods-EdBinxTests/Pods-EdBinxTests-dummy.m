d5f0725affbb7773d6e9868f03b4da76a61d791b
tush4r <stushar52@gmail.com>
2016-08-08 15:46:37 +0530

    XLPagerTabStrip with Swift 3 changes for top scrollable button bar

---
+++ EdBinx/Pods/Target Support Files/Pods-EdBinxTests/Pods-EdBinxTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EdBinxTests : NSObject
+@end
+@implementation PodsDummy_Pods_EdBinxTests
+@end
---
+++ EdBinx/Pods/Target Support Files/Pods-EdBinxTests/Pods-EdBinxTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_EdBinxTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_EdBinxTestsVersionString[];
+
