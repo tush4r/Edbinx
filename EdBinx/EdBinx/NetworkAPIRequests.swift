5fd72eebf3dca83ada1b4e207611851e382d8e04
tush4r <stushar52@gmail.com>
2016-12-09 18:46:02 +0530

    SwiftyJSON Alamofire Pods

---
+++ EdBinx/EdBinx/NetworkAPIRequests.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkAPIRequests.swift
+//  EdBinx
+//
+//  Created by Tushar Sharma on 09/12/16.
+//  Copyright © 2016 edbinx. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class APICall {
+    func getPostsCall() {
+     
+    }
+}
